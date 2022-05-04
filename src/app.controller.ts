7ec5463ea8d6780990d26045ae064d0c652d9fa0
Anh M. Tran <anh.tran@bal.com>
2022-05-04 21:27:15 +0700

    support api for case types

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import * as fs from 'fs';
 import { AppService } from './app.service';
 import { Countries } from './app.countries';
 import { CaseCategories } from './app.casecategories';
@@ -27,5 +28,12 @@ export class AppController {
   @Get('case-category')
   getCaseCategories(): any {
     return CaseCategories;
+  }
+
+  @Get('case-type')
+  // ?countryCode=${countryCode}&showArchived=false&isActive=true&includeSubTypes=false
+  getCaseTypes(@Query('countryCode') countryCode): any {
+    const json = fs.readFileSync(`./case-types/casetype.${countryCode}.json`, {encoding:'utf8', flag:'r'});
+    return JSON.parse(json);
   }
 }
