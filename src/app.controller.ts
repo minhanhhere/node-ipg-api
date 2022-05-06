f9b9a9fac0b3652471bd3197c443c864933e27a1
Anh M. Tran <anh.tran@bal.com>
2022-05-06 11:18:12 +0700

    update new api signature for case types

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -32,10 +32,19 @@ export class AppController {
 
   @Get('case-type')
   // ?countryCode=${countryCode}&showArchived=false&isActive=true&includeSubTypes=false
-  getCaseTypes(@Query('countryCode') countryCode): any {
+  getCaseTypes(@Query('countryCode') countryCode, @Query('checkAvailable') checkAvailable : boolean = false): any {
     try {
       const json = fs.readFileSync(`./case-types/casetype.${countryCode}.json`, {encoding:'utf8', flag:'r'});
-      return JSON.parse(json);
+      const caseTypes = JSON.parse(json);
+      
+      if (checkAvailable === true) {
+        for (var i = 0; i < caseTypes.length; i++) {
+          const c = caseTypes[i];
+          c.isAvailable = countryCode === 'US' && (i === 3 || i === 4 || i === 8 || i === 20) ? false : true;
+        }
+      }
+      
+      return caseTypes;
     } catch {
       return [];
     }
