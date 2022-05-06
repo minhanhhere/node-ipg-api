f109d2734ea9ed338e9b4bcc9c3a4e393e493f41
Anh M. Tran <anh.tran@bal.com>
2022-05-06 11:22:15 +0700

    cheat for boolean query params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -32,12 +32,12 @@ export class AppController {
 
   @Get('case-type')
   // ?countryCode=${countryCode}&showArchived=false&isActive=true&includeSubTypes=false
-  getCaseTypes(@Query('countryCode') countryCode, @Query('checkAvailable') checkAvailable : boolean = false): any {
+  getCaseTypes(@Query('countryCode') countryCode, @Query('checkAvailable') checkAvailable : string): any {
     try {
       const json = fs.readFileSync(`./case-types/casetype.${countryCode}.json`, {encoding:'utf8', flag:'r'});
       const caseTypes = JSON.parse(json);
       
-      if (checkAvailable === true) {
+      if (checkAvailable === 'true') {
         for (var i = 0; i < caseTypes.length; i++) {
           const c = caseTypes[i];
           c.isAvailable = countryCode === 'US' && (i === 3 || i === 4 || i === 8 || i === 20) ? false : true;
